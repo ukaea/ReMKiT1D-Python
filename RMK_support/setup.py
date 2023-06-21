ee501775bebcc3c5b0469011e7b7ad97cc4475d6
SMijin <stefan.mijin@ukaea.uk>
2023-06-21 12:45:19 +0100

    Added python-publish and final prep for 1.0.0 release

--- RMK_support/setup.py
+++ RMK_support/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages  # type: ignore
 
 setup(
     name="RMK_support",
-    version="1.0",
+    version="1.0.0",
     packages=find_packages(),
     install_requires=[
         "numpy",
