f86c2ae8a30629b2b673d52d6f2e28b0fc116f0e
SMijin <stefan.mijin@ukaea.uk>
2023-06-27 09:39:36 +0100

    Updated version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages  # type: ignore
 
 setup(
     name="RMK_support",
-    version="1.0.0",
+    version="1.0.1",
     packages=["RMK_support"],
     install_requires=[
         "numpy",
