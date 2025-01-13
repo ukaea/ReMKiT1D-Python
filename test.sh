add12c8188decb1bd6ad5be632990ece37533eb5
SMijin <stefan.mijin@ukaea.uk>
2025-01-13 17:20:24 +0000

    Slim down test.sh

--- test.sh
+++ test.sh
@@ -7,5 +7,3 @@ touch example_sources/__init__.py
 cp ../../examples/epperlein_short_test.py ./example_sources
 cp ../../examples/solkit_mijin_thesis.py ./example_sources
 python3 runner.py $@
-rm -rf example_sources
-cd $PWD
