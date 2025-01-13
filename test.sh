3f0a37dc803acb8f9506f316d4d7b9e94c22e2f6
SMijin <stefan.mijin@ukaea.uk>
2025-01-13 11:51:11 +0000

    Removed example_sources dir + black

--- test.sh
+++ test.sh
@@ -6,4 +6,5 @@ touch example_sources/__init__.py
 cp ../../examples/epperlein_short_test.py ./example_sources
 cp ../../examples/solkit_mijin_thesis.py ./example_sources
 python3 runner.py $@
+rm -rf example_sources
 cd $PWD
